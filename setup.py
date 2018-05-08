57a42e3a966b67f051af852a037537c8d1e5a48d
Raymond Ehlers <raymond.ehlers@gmail.com>
2018-05-08 14:55:26 -0400

    Add rudimentary setup.py to simplify pytest usage
    Allows the use of a subdirectory for tests. More tests coming in the
    near future

---
+++ setup.py
@@ -0,0 +1,83 @@
+#/usr/bin/env python
+
+# Setup Jet-H analysis
+# Derived from the setup.py in aliBuild and Overwatch
+# and based on: https://python-packaging.readthedocs.io/en/latest/index.html
+#
+# NOTE: We use the current directory instead of putting the modules into a folder
+#       to avoid additional distruption. This could be modified in the future.
+
+# Always prefer setuptools over distutils
+from setuptools import setup, find_packages
+# To use a consistent encoding
+from codecs import open
+from os import path
+
+# Get the long description from the README file
+here = path.abspath(path.dirname(__file__))
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name="aliceJetHadronCorrelations",
+    version="0.8",
+
+    description="ALICE jet-hadron correlations analysis",
+    long_description=long_description,
+
+    author="Raymond Ehlers",
+    author_email="raymond.ehlers@cern.ch",
+
+    url="https://github.com/ALICEYale/alice-yale-dev",
+
+    # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        'Development Status :: 4 - Beta',
+
+        # Indicate who your project is intended for
+        'Intended Audience :: Developers',
+
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate whether you support Python 2, Python 3 or both.
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.6',
+    ],
+
+    # What does your project relate to?
+    keywords='HEP ALICE',
+
+    packages=find_packages(exclude=(".git", "tests")),
+
+    # Rename scripts to the desired executable names
+    # See: https://stackoverflow.com/a/8506532
+    entry_points = {
+        "console_scripts" : [
+            "jetHAnalysis = JetHAnalysis:runFromTerminal",
+            "jetHResponse = JetHResponse:runFromTerminal",
+            "plotEMCalCorrections = EMCalCorrections:runFromTerminal",
+            "plotRPFRegions = PlotRPFRegions:runFromTerminal"
+            ],
+        },
+
+    # This is usually the minimal set of the required packages.
+    # Packages should be installed via pip -r requirements.txt !
+    install_requires=[
+        "future",
+        "ruamel.yaml",
+        "aenum",
+        "IPython",
+        "scipy",
+        "numpy",
+        "matplotlib",
+        "seaborn",
+        "rootpy",
+        "root_numpy"
+    ],
+
+    # Include additional files
+    include_package_data=True
+  )
