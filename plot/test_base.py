832a2ca1b5017c8f9ee16dea2b127d635c808512
Raymond Ehlers <raymond.ehlers@gmail.com>
2018-06-07 10:10:23 -0400

    Add docs and tests for plot.base

---
+++ plot/test_base.py
@@ -0,0 +1,109 @@
+#!/usr/bin/env python
+
+# Tests for base plotting module.
+#
+# author: Raymond Ehlers <raymond.ehlers@cern.ch>, Yale University
+# date: 6 June 2018
+
+import pytest
+import os
+import logging
+# Setup logger
+logger = logging.getLogger(__name__)
+
+import jetH.plot.base as plotBase
+
+# Set logging level as a global variable to simplify configuration.
+# This is not ideal, but fine for simple tests.
+loggingLevel = logging.DEBUG
+
+@pytest.mark.parametrize("outputPrefix,printingExtensions", [
+        ("a/b", ["png"]),
+        ("a/b", ["png", "pdf"]),
+        ("a/b/c", ["pdf"])
+    ], ids = ["standard", "multipleExtension", "variationOfOutputPrefix"]
+    )
+def testPlottingOutputWrapper(outputPrefix, printingExtensions, caplog):
+    """ Test the plottingOutputWrapper object. """
+    caplog.set_level(loggingLevel)
+    obj = plotBase.plottingOutputWrapper(outputPrefix = outputPrefix, printingExtensions = printingExtensions)
+    assert obj.outputPrefix == outputPrefix
+    assert obj.printingExtensions == printingExtensions
+
+@pytest.fixture(params = [
+        ("a/b", ["png"]),
+        ("a/b", ["png", "pdf"]),
+        ("a/b/c", ["pdf"])
+    ], ids = ["standard", "multipleExtension", "variationOfOutputPrefix"]
+    )
+def setupSaveTests(request, mocker):
+    """ Provides mock objects for testing the save oriented functionality. """
+    (outputPrefix, printingExtensions) = request.param
+    obj = mocker.MagicMock(spec = ["outputPrefix", "printingExtensions"],
+            outputPrefix = outputPrefix,
+            printingExtensions = printingExtensions)
+    # Only the functions in the spec are allowed, which is why the objects are separate.
+    figure = mocker.MagicMock(spec = ["savefig"])
+    canvas = mocker.MagicMock(spec = ["SaveAs"])
+
+    expectedFilenames = []
+    for ext in printingExtensions:
+        expectedFilenames.append(os.path.join(outputPrefix, "{filename}." + ext))
+    return (obj, figure, canvas, expectedFilenames)
+
+def testSavePlot(caplog, setupSaveTests):
+    """ Test the wrapper for saving a matplotlib plot. """
+    caplog.set_level(loggingLevel)
+    (obj, figure, canvas, expectedFilenames) = setupSaveTests
+    filename = "filename"
+    filenames = plotBase.savePlot(obj, figure, filename)
+    expectedFilenames = [name.format(filename = filename) for name in expectedFilenames]
+    assert filenames == expectedFilenames
+ 
+def testSaveCanvas(caplog, setupSaveTests):
+    """ Test the wrapper for saving a ROOT canvas. """
+    caplog.set_level(loggingLevel)
+    (obj, figure, canvas, expectedFilenames) = setupSaveTests
+    filename = "filename"
+    filenames = plotBase.saveCanvas(obj, canvas, filename)
+    expectedFilenames = [name.format(filename = filename) for name in expectedFilenames]
+    assert filenames == expectedFilenames
+
+def testSavePlotImpl(caplog, setupSaveTests):
+    """ Test the implementation for saving a matplotlib plot. """
+    caplog.set_level(loggingLevel)
+    (obj, figure, canvas, expectedFilenames) = setupSaveTests
+    filename = "filename"
+    filenames = plotBase.savePlotImpl(figure, obj.outputPrefix, filename, obj.printingExtensions)
+    expectedFilenames = [name.format(filename = filename) for name in expectedFilenames]
+    assert filenames == expectedFilenames
+
+def testSaveCanvasImpl(caplog, mocker, setupSaveTests):
+    """ Test the implementation for saving a ROOT canvas. """
+    caplog.set_level(loggingLevel)
+    (obj, figure, canvas, expectedFilenames) = setupSaveTests
+    filename = "filename"
+    filenames = plotBase.saveCanvasImpl(canvas, obj.outputPrefix, filename, obj.printingExtensions)
+    expectedFilenames = [name.format(filename = filename) for name in expectedFilenames]
+    assert filenames == expectedFilenames
+
+def testRegistrationOfKBirdColormap(caplog):
+    """ Test to ensure that the ROOT kBird colormap is registered in matplotlib successfully. """
+    import matplotlib.pyplot as plt
+    kBirdName = "ROOT_kBird"
+    # If it doesn't exist, it will throw an exception, which will fail the test
+    kBird = plt.get_cmap(kBirdName)
+
+    assert kBird.name == kBirdName
+
+def testFixBadColormapValue(caplog):
+    """ Test that the bad (ie. nan or similar) value of a given color scheme is successfully reassigned. """
+    import matplotlib.pyplot as plt
+    cmap = plt.get_cmap("viridis")
+    # NOTE: There isn't any direct access to this property.
+    initialBadValue = cmap._rgba_bad
+    cmap = plotBase.prepareColormap(cmap)
+    finalBadValue = cmap._rgba_bad
+
+    assert initialBadValue != finalBadValue
+    assert finalBadValue == (1.0, 1.0, 1.0, 1.0)
