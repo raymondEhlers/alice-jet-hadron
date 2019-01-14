f3a91f61804a66b05edc6b537cc7049934a8d666
Raymond Ehlers <raymond.ehlers@gmail.com>
2019-01-13 21:22:46 -0500

    Some improvements before full overall

--- jet_hadron/analysis/fit.py
+++ jet_hadron/analysis/fit.py
@@ -4,21 +4,15 @@
 from future.utils import itervalues
 from future.utils import iteritems
 
-import math
-# Import for convenience when defining the RP functions
-from math import sin, cos
-import sys
-import time
-import os
-import copy
 import collections
+#import IPython
 import itertools
 import logging
-logger = logging.getLogger(__name__)
-
-import IPython
-
+from math import sin, cos
 import numpy as np
+import sys
+import time
+from typing import Sequence
 
 # Modules for fits
 import iminuit
@@ -29,211 +23,231 @@ import numdifftools as nd
 from jet_hadron.base import params
 from jet_hadron.base import analysis_objects
 from jet_hadron.base import analysis_config
+from jet_hadron.base.typing_helpers import Hist
 from jet_hadron.plot import fit as plotFit
 
-import rootpy.ROOT as ROOT
+import ROOT
 
-def fitDeltaPhiBackground(hist, trackPtBin, zyam = True, disableVN = True, setFixedVN = False):
+logger = logging.getLogger(__name__)
+
+# Shared values (TODO: to be removed...)
+v2_cent_00_05_values = [1, 2, 3]
+v2_cent_05_10_values = [1, 2, 3]
+
+def fit_delta_phi_background(hist: Hist, track_pt: analysis_objects.TrackPtBin, zyam: bool = True, disable_vn: bool = True, set_fixed_vn: bool = False) -> ROOT.TF1:
     """ Fit the delta phi background. """
-    deltaPhi = hist
+    delta_phi = hist
 
-    fitFunc = ROOT.TF1("deltaPhiBackground", "[0]*1 + (2*[1]*cos(2*x) + 2*[2]*cos(3*x))",
-                        -0.5*ROOT.TMath.Pi(), 1.5*ROOT.TMath.Pi())
+    fit_func = ROOT.TF1(
+        "deltaPhiBackground",
+        "[0]*1 + (2*[1]*cos(2*x) + 2*[2]*cos(3*x))",
+        -0.5 * ROOT.TMath.Pi(), 1.5 * ROOT.TMath.Pi()
+    )
 
     # Backgound
     # Pedestal
-    fitFunc.SetParLimits(0, 0.,100)
+    fit_func.SetParLimits(0, 0., 100)
     if zyam:
         # Seed with ZYAM value
-        fitFunc.SetParameter(0, deltaPhi.GetBinContent(deltaPhi.GetMinimumBin()))
-    fitFunc.SetParName(0, "Pedestal")
+        fit_func.SetParameter(0, delta_phi.GetBinContent(delta_phi.GetMinimumBin()))
+    fit_func.SetParName(0, "Pedestal")
     # v2
-    fitFunc.SetParLimits(1, -1, 1)
-    fitFunc.SetParName(1, "v_{2}")
+    fit_func.SetParLimits(1, -1, 1)
+    fit_func.SetParName(1, "v_{2}")
     # v3
-    fitFunc.SetParLimits(2, -1, 1)
-    fitFunc.SetParName(2, "v_{3}")
-    if disableVN:
-        fitFunc.FixParameter(1, 0)
-        fitFunc.FixParameter(2, 0)
-    if setFixedVN:
-        #v2Assoc = math.sqrt((v2Cent00_05Values[trackPtBin] + v2Cent05_10Values[trackPtBin])/2.)
-        v2Assoc = (v2Cent00_05Values[trackPtBin] + v2Cent05_10Values[trackPtBin])/2.
+    fit_func.SetParLimits(2, -1, 1)
+    fit_func.SetParName(2, "v_{3}")
+    if disable_vn:
+        fit_func.FixParameter(1, 0)
+        fit_func.FixParameter(2, 0)
+    if set_fixed_vn:
+        raise ValueError("v2_assoc must be properly defined!")
+        v2_assoc = (v2_cent_00_05_values[track_pt.bin] + v2_cent_05_10_values[track_pt.bin]) / 2.
+        #v2_assoc = math.sqrt((v2_cent_00_05_values[track_pt.bin] + v2_cent_05_10_values[track_pt.bin]) / 2.)
         # From https://arxiv.org/pdf/1509.07334v2.pdf
-        v2Jet = 0.03
-        v3Assoc = 0
-        v3Jet = v3Assoc
-        fitFunc.FixParameter(7, v2Assoc*v2Jet)
-        fitFunc.FixParameter(8, v3Assoc*v3Jet)
+        v2_jet = 0.03
+        v3_assoc = 0
+        v3_jet = v3_assoc
+        fit_func.FixParameter(7, v2_assoc * v2_jet)
+        fit_func.FixParameter(8, v3_assoc * v3_jet)
 
     # Set styling
-    fitFunc.SetLineColor(ROOT.kBlue+2)
-    fitFunc.SetLineStyle(1)
+    fit_func.SetLineColor(ROOT.kBlue + 2)
+    fit_func.SetLineStyle(1)
 
     # Fit to the given histogram
     # R uses the range defined in the fit function
     # 0 ensures that the fit isn't drawn
     # Q ensures minimum printing
     # + adds the fit to function list to ensure that it is not deleted on the creation of a new fit
-    deltaPhi.Fit(fitFunc, "RIB0")
+    delta_phi.Fit(fit_func, "RIB0")
 
-    return fitFunc
+    return fit_func
 
-def fitDeltaPhi(hist, trackPtBin, zyam = True, disableVN = True, setFixedVN = False):
+def fit_delta_phi(hist: Hist, track_pt: analysis_objects.TrackPtBin, zyam: bool = True, disable_vn: bool = True, set_fixed_vn: bool = False) -> ROOT.TF1:
     """ Define 1D gaussian fit function with one gaussian each for the near and away sides, along with a gaussian offset by +/-2Pi"""
-    deltaPhi = hist
+    delta_phi = hist
 
-    #fitFunc = ROOT.TF1("symmetricGaussian","[0]*exp(-0.5*((x-[1])/[2])**2)+[3]+[4]*exp(-0.5*((x-[5])/[6])**2)+[0]*exp(-0.5*((x-[1]+2.*TMath::Pi())/[2])**2)+[4]*exp(-0.5*((x-[5]-2.*TMath::Pi())/[6])**2)", -0.5*ROOT.TMath.Pi(), 1.5*ROOT.TMath.Pi())
+    #fit_func = ROOT.TF1("symmetricGaussian","[0]*exp(-0.5*((x-[1])/[2])**2)+[3]+[4]*exp(-0.5*((x-[5])/[6])**2)+[0]*exp(-0.5*((x-[1]+2.*TMath::Pi())/[2])**2)+[4]*exp(-0.5*((x-[5]-2.*TMath::Pi())/[6])**2)", -0.5*ROOT.TMath.Pi(), 1.5*ROOT.TMath.Pi())
     # NOTE: This is not symmetric! Instead, the extra fits are because of how it wraps around. Even if our data doesn't go there, it is still relevant
-    fitFunc = ROOT.TF1("dPhiWithGaussians",
-                        "[6]*1 + (2*[7]*cos(2*x) + 2*[8]*cos(3*x)) + "
-                        "[0]*(TMath::Gaus(x, [1], [2]) + TMath::Gaus(x, [1]-2.*TMath::Pi(), [2]) + TMath::Gaus(x, [1]+2.*TMath::Pi(), [2])) + "
-                        "[3]*(TMath::Gaus(x, [4], [5]) + TMath::Gaus(x, [4]-2.*TMath::Pi(), [5]) + TMath::Gaus(x, [4]+2.*TMath::Pi(), [5]))",
-                        -0.5*ROOT.TMath.Pi(), 1.5*ROOT.TMath.Pi())
+    fit_func = ROOT.TF1(
+        "dPhiWithGaussians",
+        "[6]*1 + (2*[7]*cos(2*x) + 2*[8]*cos(3*x)) + "
+        "[0]*(TMath::Gaus(x, [1], [2]) + TMath::Gaus(x, [1]-2.*TMath::Pi(), [2]) + TMath::Gaus(x, [1]+2.*TMath::Pi(), [2])) + "
+        "[3]*(TMath::Gaus(x, [4], [5]) + TMath::Gaus(x, [4]-2.*TMath::Pi(), [5]) + TMath::Gaus(x, [4]+2.*TMath::Pi(), [5]))",
+        -0.5 * ROOT.TMath.Pi(), 1.5 * ROOT.TMath.Pi()
+    )
 
     # Setup parameters
-    amplitudeLimits = [0.0, 100.0]
-    sigmaLimits = [0.05, 2.0]
+    amplitude_limits = [0.0, 100.0]
+    sigma_limits = [0.05, 2.0]
     # Near side
     # Amplitude
-    fitFunc.SetParLimits(0, amplitudeLimits[0], amplitudeLimits[1])
-    fitFunc.SetParName(0, "NS Amplitude")
+    fit_func.SetParLimits(0, amplitude_limits[0], amplitude_limits[1])
+    fit_func.SetParName(0, "NS Amplitude")
     # Offset
-    fitFunc.FixParameter(1, 0)
-    fitFunc.SetParName(1, "NS Offset")
+    fit_func.FixParameter(1, 0)
+    fit_func.SetParName(1, "NS Offset")
     # Sigma
-    fitFunc.SetParLimits(2, sigmaLimits[0], sigmaLimits[1])
-    fitFunc.SetParName(2, "NS #sigma")
+    fit_func.SetParLimits(2, sigma_limits[0], sigma_limits[1])
+    fit_func.SetParName(2, "NS #sigma")
     # Seed for sigma
-    fitFunc.SetParameter(2, sigmaLimits[0])
+    fit_func.SetParameter(2, sigma_limits[0])
 
     # Away side
     # Amplitude
-    fitFunc.SetParLimits(3, amplitudeLimits[0], amplitudeLimits[1])
-    fitFunc.SetParName(3, "AS Amplitude")
+    fit_func.SetParLimits(3, amplitude_limits[0], amplitude_limits[1])
+    fit_func.SetParName(3, "AS Amplitude")
     # Offset
-    fitFunc.FixParameter(4, ROOT.TMath.Pi())
-    fitFunc.SetParName(4, "AS Offset")
+    fit_func.FixParameter(4, ROOT.TMath.Pi())
+    fit_func.SetParName(4, "AS Offset")
     # Sigma
-    fitFunc.SetParLimits(5, sigmaLimits[0], sigmaLimits[1])
-    fitFunc.SetParName(5, "AS #sigma")
+    fit_func.SetParLimits(5, sigma_limits[0], sigma_limits[1])
+    fit_func.SetParName(5, "AS #sigma")
     # Seed for sigma
-    fitFunc.SetParameter(5, sigmaLimits[0])
+    fit_func.SetParameter(5, sigma_limits[0])
 
     # Backgound
     # Pedestal
-    fitFunc.SetParLimits(6, 0.,100)
+    fit_func.SetParLimits(6, 0., 100)
     if zyam:
         # Seed with ZYAM value
-        fitFunc.SetParameter(6, deltaPhi.GetBinContent(deltaPhi.GetMinimumBin()))
-    fitFunc.SetParName(6, "Pedestal")
+        fit_func.SetParameter(6, delta_phi.GetBinContent(delta_phi.GetMinimumBin()))
+    fit_func.SetParName(6, "Pedestal")
     # v2
-    fitFunc.SetParLimits(7, -1, 1)
-    fitFunc.SetParName(7, "v_{2}")
+    fit_func.SetParLimits(7, -1, 1)
+    fit_func.SetParName(7, "v_{2}")
     # v3
-    fitFunc.SetParLimits(8, -1, 1)
-    fitFunc.SetParName(8, "v_{3}")
-    if disableVN:
-        fitFunc.FixParameter(7, 0)
-        fitFunc.FixParameter(8, 0)
-    if setFixedVN:
-        #v2Assoc = math.sqrt((v2Cent00_05Values[trackPtBin] + v2Cent05_10Values[trackPtBin])/2.)
-        v2Assoc = (v2Cent00_05Values[trackPtBin] + v2Cent05_10Values[trackPtBin])/2.
+    fit_func.SetParLimits(8, -1, 1)
+    fit_func.SetParName(8, "v_{3}")
+    if disable_vn:
+        fit_func.FixParameter(7, 0)
+        fit_func.FixParameter(8, 0)
+    if set_fixed_vn:
+        raise ValueError("v2_assoc must be properly defined!")
+        v2_assoc = (v2_cent_00_05_values[track_pt.bin] + v2_cent_05_10_values[track_pt.bin]) / 2.
+        #v2_assoc = math.sqrt((v2_cent_00_05_values[track_pt.bin] + v2_cent_05_10_values[track_pt.bin]) / 2.)
         # From https://arxiv.org/pdf/1509.07334v2.pdf
-        v2Jet = 0.03
-        v3Assoc = 0
-        v3Jet = v3Assoc
-        #fitFunc.SetParameter(7, v2Assoc*v2Jet)
-        fitFunc.FixParameter(7, v2Assoc*v2Jet)
-        fitFunc.FixParameter(8, v3Assoc*v3Jet)
+        v2_jet = 0.03
+        v3_assoc = 0
+        v3_jet = v3_assoc
+        #fit_func.SetParameter(7, v2_assoc * v2_jet)
+        fit_func.FixParameter(7, v2_assoc * v2_jet)
+        fit_func.FixParameter(8, v3_assoc * v3_jet)
 
     # Set styling
-    fitFunc.SetLineColor(ROOT.kRed + 2)
-    fitFunc.SetLineStyle(1)
+    fit_func.SetLineColor(ROOT.kRed + 2)
+    fit_func.SetLineStyle(1)
 
     # Fit to the given histogram
     # R uses the range defined in the fit function
     # 0 ensures that the fit isn't drawn
     # Q ensures minimum printing
     # + adds the fit to function list to ensure that it is not deleted on the creation of a new fit
-    deltaPhi.Fit(fitFunc, "RIB0")
+    delta_phi.Fit(fit_func, "RIB0")
 
     # And return the fit
-    return fitFunc
+    return fit_func
 
-def fitDeltaEta(hist, trackPtBin, zyam = True, disableVN = True, setFixedVN = False):
+def fit_delta_eta(hist: Hist, track_pt: analysis_objects.TrackPtBin, zyam: bool = True, disable_vn: bool = True, set_fixed_vn: bool = False) -> ROOT.TF1:
     """ dEta near-side fit implementation. """
-    fitFunc = ROOT.TF1("deltaEtaNS", "[6] + [0]*TMath::Gaus(x, [1], [2])", -1, 1)
+    fit_func = ROOT.TF1("deltaEtaNS", "[6] + [0]*TMath::Gaus(x, [1], [2])", -1, 1)
 
     # Setup parameters
-    amplitudeLimits = [0.0, 100.0]
-    sigmaLimits = [0.05, 2.0]
+    amplitude_limits = [0.0, 100.0]
+    sigma_limits = [0.05, 2.0]
     # Near side
     # Amplitude
-    fitFunc.SetParLimits(0, amplitudeLimits[0], amplitudeLimits[1])
-    fitFunc.SetParName(0, "NS Amplitude")
+    fit_func.SetParLimits(0, amplitude_limits[0], amplitude_limits[1])
+    fit_func.SetParName(0, "NS Amplitude")
     # Offset
-    fitFunc.FixParameter(1, 0)
-    fitFunc.SetParName(1, "NS Offset")
+    fit_func.FixParameter(1, 0)
+    fit_func.SetParName(1, "NS Offset")
     # Sigma
-    fitFunc.SetParLimits(2, sigmaLimits[0], sigmaLimits[1])
-    fitFunc.SetParName(2, "NS #sigma")
+    fit_func.SetParLimits(2, sigma_limits[0], sigma_limits[1])
+    fit_func.SetParName(2, "NS #sigma")
     # Seed for sigma
-    fitFunc.SetParameter(2, sigmaLimits[0])
+    fit_func.SetParameter(2, sigma_limits[0])
 
     # Backgound
-    fitFunc.SetParLimits(6, 0.,100)
+    fit_func.SetParLimits(6, 0., 100)
     if zyam:
         # Seed with ZYAM value
-        fitFunc.SetParameter(6, hist.GetBinContent(hist.GetMinimumBin()))
-    fitFunc.SetParName(6, "Pedestal")
+        fit_func.SetParameter(6, hist.GetBinContent(hist.GetMinimumBin()))
+    fit_func.SetParName(6, "Pedestal")
 
     # Set styling
-    fitFunc.SetLineColor(ROOT.kGreen + 2)
-    fitFunc.SetLineStyle(1)
+    fit_func.SetLineColor(ROOT.kGreen + 2)
+    fit_func.SetLineStyle(1)
 
     # Fit to the given histogram
     # R uses the range defined in the fit function
     # 0 ensures that the fit isn't drawn
     # Q ensures minimum printing
     # + adds the fit to function list to ensure that it is not deleted on the creation of a new fit
-    hist.Fit(fitFunc, "RIB0")
+    hist.Fit(fit_func, "RIB0")
 
     # And return the fit
-    return fitFunc
+    return fit_func
 
-def fit1DMixedEventNormalization(hist, dPhiLimits):
+def fit_1d_mixed_event_normalization(hist: Hist, delta_phi_limits: Sequence[float]) -> ROOT.TF1:
     """ Alternative to determine the mixed event normalization.
 
-    A lienar function is fit to the dPhi mixed event normalization for some predefined range. """
-    fitFunc = ROOT.TF1("mixedEventNormalization1D", "[0] + 0.0*x", dPhiLimits[0], dPhiLimits[1])
+    A lienar function is fit to the dPhi mixed event normalization for some predefined range.
+    """
+    fit_func = ROOT.TF1("mixedEventNormalization1D", "[0] + 0.0*x", delta_phi_limits[0], delta_phi_limits[1])
 
     # Fit to the given histogram
     # R uses the range defined in the fit function
     # 0 ensures that the fit isn't drawn
     # Q ensures minimum printing
     # + adds the fit to function list to ensure that it is not deleted on the creation of a new fit
-    hist.Fit(fitFunc, "RIB0")
+    hist.Fit(fit_func, "RIB0")
 
     # And return the fit
-    return fitFunc
+    return fit_func
 
-def fit2DMixedEventNormalization(hist, dPhiLimits, dEtaLimits):
+def fit_2d_mixed_event_normalization(hist: Hist, delta_phi_limits: Sequence[float], delta_eta_limits: Sequence[float]) -> ROOT.TF2:
     """ Alternative to determine the mixed event normalization.
 
-    A lienar function is fit to the dPhi-dEta mixed event normalization for some predefined range. """
-    fitFunc = ROOT.TF2("mixedEventNormalization2D", "[0] + 0.0*x + 0.0*y", dPhiLimits[0], dPhiLimits[1],
-                                                                           dEtaLimits[0], dEtaLimits[1])
+    A lienar function is fit to the dPhi-dEta mixed event normalization for some predefined range.
+    """
+    fit_func = ROOT.TF2(
+        "mixedEventNormalization2D",
+        "[0] + 0.0*x + 0.0*y",
+        delta_phi_limits[0], delta_phi_limits[1],
+        delta_eta_limits[0], delta_eta_limits[1]
+    )
 
     # Fit to the given histogram
     # R uses the range defined in the fit function
     # 0 ensures that the fit isn't drawn
     # Q ensures minimum printing
     # + adds the fit to function list to ensure that it is not deleted on the creation of a new fit
-    hist.Fit(fitFunc, "RIB0")
+    hist.Fit(fit_func, "RIB0")
 
     # And return the fit
-    return fitFunc
+    return fit_func
 
 #####################
 # Reaction Plane Fit
@@ -281,8 +295,8 @@ class JetHEPFit(object):
 
     def GetFitFunction(self, fitType, epAngle):
         """ Simple wrapper to get the fit function corresponding to the selected fitType. """
-        fitFunctionMap = {analysis_objects.JetHCorrelationType.signal_dominated : GetSignalDominatedFitFunction,
-                          analysis_objects.JetHCorrelationType.background_dominated : GetBackgroundDominatedFitFunction}
+        fitFunctionMap = {analysis_objects.JetHCorrelationType.signal_dominated: GetSignalDominatedFitFunction,
+                          analysis_objects.JetHCorrelationType.background_dominated: GetBackgroundDominatedFitFunction}
 
         # The fit container stores the fit type as a string. We need it as a JetHCorrelationType
         if isinstance(fitType, str):
