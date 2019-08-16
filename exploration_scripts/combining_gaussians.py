6babbe79e9474db9cdda1dadbc9e5962f46c53c7
Raymond Ehlers <raymond.ehlers@gmail.com>
2019-08-16 01:45:03 -0400

    Some addition explorations
    Should re-enable n_trig scaling and need to explore the small difference
    in widths limit, but this looks fairly promising

--- exploration_scripts/combining_gaussians.py
+++ exploration_scripts/combining_gaussians.py
@@ -5,10 +5,13 @@
 .. codeauthor:: Raymond Ehlers <raymond.ehlers@cern.ch>, Yale University
 """
 
+from typing import Dict, Tuple, Union
+
 import matplotlib.pyplot as plt
 import numpy as np
 import scipy.stats
-from typing import Tuple
+
+from pachyderm import fit, histogram
 
 def create_gaussian_inputs() -> Tuple[np.ndarray, np.ndarray]:
     g1 = np.random.normal(0, 1, size = 1000000)
@@ -56,15 +59,22 @@ def new_combine_gaussians() -> None:
     # Imagine with 3 EP angles + inclusive
     # First define the 3 EP angles
     gaussians = []
-    for width in range(1, 4):
+    #for width in range(1, 4):
+    for width in [0.95, 1.0, 1.05]:
         gaussians.append(np.random.normal(0, width, size = 1000000))
-    #n_trig = [600, 300, 100]
+    n_trigs = [375, 300, 325]
+    # Add the inclusive to the start of the number of trigs
+    n_trigs.insert(0, np.sum(n_trigs))
 
     # Create the inclusive, and then histogram it
     inclusive = np.array([(i, val) for i, gaussian in enumerate(gaussians, 1) for val in gaussian])
 
-    h_inclusive, x_bin_edges, y_bin_edges = np.histogram2d(inclusive[:, 1], inclusive[:, 0], bins = [200, np.linspace(0.5, 3.5, 3 + 1)], range = [[-10, 10], [0.5, 3.5]])
+    # Recall that [:, 0] contains 1-3, while [:, 1] contains the x values.
+    h_inclusive, x_bin_edges, y_bin_edges = np.histogram2d(
+        inclusive[:, 1], inclusive[:, 0], bins = [200, np.linspace(0.5, 3.5, 3 + 1)], range = [[-10, 10], [0.5, 3.5]]
+    )
 
+    print(f"h_inclusive.shape: {h_inclusive.shape}")
     #print(f"x_bin_edges: {x_bin_edges}")
     #print(f"y_bin_edges: {y_bin_edges}")
     print(f"inclusive[:, 0]: {inclusive[:, 0]}")
@@ -80,14 +90,88 @@ def new_combine_gaussians() -> None:
     fig.savefig("gaussian_2d_histogram.pdf")
 
     # Basically, the first two arguments are h.x and h.y
-    binned_mean = scipy.stats.binned_statistic(
+    binned_mean, _, _ = scipy.stats.binned_statistic(
         inclusive[:, 0], inclusive[:, 1], "std", bins = np.linspace(0.5, 3.5, 3 + 1)
     )
     print(f"Binned mean: {binned_mean}")
-    inclusive_binned_mean = scipy.stats.binned_statistic(inclusive[:, 0], inclusive[:, 1], "std", bins = 1)
+    inclusive_binned_mean, _, _ = scipy.stats.binned_statistic(inclusive[:, 0], inclusive[:, 1], "std", bins = 1)
     print(f"Inclusive binned mean: {inclusive_binned_mean}")
 
+    hists = []
+    # Inclusive
+    hists.append(histogram.Histogram1D(
+        bin_edges = x_bin_edges, y = np.sum(h_inclusive, axis = 1), errors_squared = np.sum(h_inclusive[:, 0]),
+    ))
+    # Width = 1
+    hists.append(histogram.Histogram1D(
+        bin_edges = x_bin_edges, y = h_inclusive[:, 0], errors_squared = np.copy(h_inclusive[:, 0]),
+    ))
+    # Width = 2
+    hists.append(histogram.Histogram1D(
+        bin_edges = x_bin_edges, y = h_inclusive[:, 1], errors_squared = np.copy(h_inclusive[:, 1]),
+    ))
+    # Width = 3
+    hists.append(histogram.Histogram1D(
+        bin_edges = x_bin_edges, y = h_inclusive[:, 2], errors_squared = np.copy(h_inclusive[:, 2]),
+    ))
+
+    # Scale by number of triggers.
+    #for h, n_trig in zip(hists, n_trigs):
+    #for i, n_trig in enumerate(n_trigs):
+    #    print(f" pre scale {i}: {np.max(hists[i].y)}")
+    #    print(f"scale by {1 / n_trig}")
+    #    hists[i] *= 1.0 / n_trig
+    #    #h = h * 1.0 / n_trig
+    #    print(f"post scale {i}: {np.max(hists[i].y)}")
+
+    # Quickly plot hists
+    fig, ax = plt.subplots(figsize = (8, 6))
+    for i, h in enumerate(hists):
+        ax.errorbar(h.x, h.y, yerr = h.errors, marker = "o", linestyle = "", label = f"Data {i}")
+    # Final adjustments
+    ax.legend()
+    fig.tight_layout()
+    fig.savefig(f"gaussian_data.pdf")
+
+    # Fit to gaussians
+    def scaled_gaussian(x: float, mean: float, sigma: float, amplitude: float) -> float:
+        return amplitude * fit.gaussian(x = x, mean = mean, sigma = sigma)
+
+    fig, ax = plt.subplots(figsize = (8, 6))
+    gaussian_fit_results = []
+    for i, h in enumerate(hists):
+        cost_func = fit.BinnedLogLikelihood(f = scaled_gaussian, data = h)
+        minuit_args: Dict[str, Union[float, Tuple[float, float]]] = {
+            "mean": 0, "fix_mean": True,
+            "sigma": 1.0, "error_sigma": 0.1, "limit_sigma": (0, 100),
+            "amplitude": 100.0, "error_amplitude": 0.1,
+        }
+        fit_result, _ = fit.fit_with_minuit(
+            cost_func = cost_func, minuit_args = minuit_args, log_likelihood = True, x = h.x
+        )
+        gaussian_fit_results.append(fit_result)
+
+        # Plot for a sanity check
+        ax.errorbar(h.x, h.y, yerr = h.errors, marker = "o", linestyle = "", label = f"Data {i}")
+        ax.plot(h.x, scaled_gaussian(h.x, *list(fit_result.values_at_minimum.values())), label = f"Fit {i}", zorder = 5)
+
+    # Final adjustments
+    ax.legend()
+    fig.tight_layout()
+    fig.savefig(f"gaussian_fit.pdf")
+
+    values_at_zero_from_hist = []
+    for h in hists:
+        values_at_zero_from_hist.append(h.y[h.find_bin(0.0)])
+    values_at_zero_from_fits = []
+    for fit_result in gaussian_fit_results:
+        values_at_zero_from_fits.append(scaled_gaussian(0, *list(fit_result.values_at_minimum.values())))
+    print(f"Values at 0 from hist: {values_at_zero_from_hist}, Sum of last 3: {np.sum(values_at_zero_from_hist[1:])}")
+    print(f"Values at 0 from fit: {values_at_zero_from_fits}, Sum of last 3: {np.sum(values_at_zero_from_fits[1:])}")
+
 if __name__ == "__main__":
+    print("---- New explorations ----")
     new_combine_gaussians()
+    print("---- Older explorations -----")
     combine_gaussians()
 
